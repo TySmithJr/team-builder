1fb0a623a916ffe6e3a6c5ba62e50f476620b545
Tyrone Smith <billionairefly.ts@gmail.com>
2019-07-16 10:01:37 -0400

    created a changeHandler

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -2,17 +2,18 @@ import React, { useState } from 'react';
 import './App.css';
 import { TeamData } from "./TeamData";
 import ListOfPlayers from "./Components/ListOfPlayers";
+import Form from "./Components/Form"
 // import TeamMembers from "./Components/TeamMembers";
 
 function App() {
   
-  const [members, SetMembers] = useState(TeamData)
+  const [members, setMembers] = useState(TeamData)
   
 
   return (
     <div className="App">
       <ListOfPlayers  members={members}/>
-      
+      <Form setMembers={setMembers}/>
     </div>
   );
 }
--- team-builder/src/Components/Form.js
+++ team-builder/src/Components/Form.js
@@ -1,20 +1,27 @@
 import React, {useState} from "react";
 
 function Form() {
-    const [storeData, setStoreData] = useState()
+    const [input, setInput] = useState({name: "", email: "", position: ""})
+            console.log("input", input)
 
+            const changeHandler = event => {
+                setInput({...input, [event.target.name]: event.target.value})
+            }
 
     return (
         <div>
             <form>
                 <input
                 placeholder="name"
+                name= "name"
                 />
                 <input
                 placeholder="email"
+                name= "email"
                 />
                 <input
                 placeholder="position"
+                name= "position"
                 />
             </form>
         </div>
